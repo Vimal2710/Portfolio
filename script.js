2f60bfcc8cc043945d546f70a40caf80ee8c6b71
Vimal A <139349324+Vimal2710@users.noreply.github.com>
2024-03-11 14:09:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
+// 
+{/*     <div class="cer">
+        <p>
+        <strong>Udemy - </strong>Learn HTML 5: The Complete HTML 5 And CSS3 Tutorials Course
+        </p>
+        <img
+        src="link.png"
+        alt="Arrow icon"
+        class="icon arrow"
+        onclick="location.href='./#'"
+      />
+      </div>
+      <div class="cer">
+        <p><strong>Udemy - </strong>Learn HTML 5: The Complete HTML 5 And CSS3 Tutorials Course</p>
+        <img
+        src="link.png"
+        alt="Arrow icon"
+        class="icon arrow"
+        onclick="location.href='./#'"
+      />
+      </div>
+      <div class="cer">
+        <p><strong>NPTEL - </strong>Internet Of Things</p>
+        <img
+        src="link.png"
+        alt="Arrow icon"
+        class="icon arrow"
+        onclick="location.href='./#'"
+      />
+      </div>
+      <div class="cer">
+        <p><strong>HackerRank - </strong>SQL (Intermediate) Certificate</p>
+        <img
+        src="link.png"
+        alt="Arrow icon"
+        class="icon arrow"
+        onclick="location.href='./#'"
+      />
+      </div>
+      <div class="cer">
+<p><strong>Oxford University Press - </strong>Oxford Achiever English Assessment Program</p>
+<img
+src="link.png"
+alt="Arrow icon"
+class="icon arrow"
+onclick="location.href='./#'"
+/>
+</div>
+<div class="cer">
+<p><strong>Intra College Event - </strong>Won 1st price in Line follower and Hover boat events in Robofest in the
+  academic years 2022 and 2023.</p>
+<img
+src="link.png"
+alt="Arrow icon"
+class="icon arrow"
+onclick="location.href='./#'"
+/>
+</div>
+<div class="cer">
+<p><strong>NSS - </strong>Volunteer in School Adoption Program</p>
+<img
+src="link.png"
+alt="Arrow icon"
+class="icon arrow"
+onclick="location.href='./#'"
+/>
+</div> */}
